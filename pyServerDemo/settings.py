4558be86b58f0fb882ea9954e1de18cf18f0d3e6
DJTakushi <okinawanspud@gmail.com>
2022-07-28 15:39:38 -0500

    PostgreSql conversion attempt
    https://python.plainenglish.io/deploy-your-django-app-to-heroku-with-postgresql-eb365ec5eaef

--- pyServerDemo/settings.py
+++ pyServerDemo/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -77,8 +77,8 @@ WSGI_APPLICATION = 'pyServerDemo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pyServerDemo',
     }
 }
 
@@ -124,3 +124,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# Activate Django-Heroku.
+django_heroku.settings(locals())
