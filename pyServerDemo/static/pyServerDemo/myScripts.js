434765090c1180dcd59dd723ff87af53c5cf07f1
DJTakushi <okinawanspud@gmail.com>
2022-08-13 16:19:18 -0500

    themes added, styling improved

--- pyServerDemo/static/pyServerDemo/myScripts.js
+++ pyServerDemo/static/pyServerDemo/myScripts.js
@@ -14,7 +14,31 @@ function setActiveNavLink(id){
     element.className+=" nav-link-active";
   }
   else{
-    console.log("setActiveNavLink could not find the id to set active(?)")    
+    console.log("setActiveNavLink could not find the id to set active(?)")
   }
   console.log("ran setActiveNavLink")
 }
+
+// function to set a given theme/color-scheme
+// from https://medium.com/@haxzie/dark-and-light-theme-switcher-using-css-variables-and-pure-javascript-zocada-dd0059d72fa2
+function setTheme(themeName) {
+    localStorage.setItem('theme', themeName);
+    document.documentElement.className = themeName;
+    console.log("document.documentElement.className = ",document.documentElement.className)
+}
+// function to toggle between light and dark theme
+function toggleTheme() {
+   if (localStorage.getItem('theme') === 'theme-dark'){
+       setTheme('theme-light');
+   } else {
+       setTheme('theme-dark');
+   }
+}
+// Immediately invoked function to set the theme on initial load
+(function () {
+   if (localStorage.getItem('theme') === 'theme-dark') {
+       setTheme('theme-dark');
+   } else {
+       setTheme('theme-light');
+   }
+})();
