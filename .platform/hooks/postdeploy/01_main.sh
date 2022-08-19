61dd5c7fd4cdb14712a5b6149d18308e9bc29f6b
DJTakushi <okinawanspud@gmail.com>
2022-08-19 16:56:17 -0500

    CRLF to LF

--- .platform/hooks/postdeploy/01_main.sh
+++ .platform/hooks/postdeploy/01_main.sh
@@ -3,4 +3,4 @@
 source /var/app/venv/staging-LQM1lest/bin/activate
 python /var/app/current/manage.py migrate
 python /var/app/current/manage.py createsu
-python /var/app/current/manage.py collectstatic --noinput
+python /var/app/current/manage.py collectstatic --noinput 
