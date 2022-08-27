10cfa0f782b6e77fdeeab8feac647adb904efc88
DJTakushi <okinawanspud@gmail.com>
2022-08-27 14:43:44 -0500

    remove recursive file search, properly index filename parsing, and ignore orientation data

--- scripts/img.py
+++ scripts/img.py
@@ -1,17 +1,22 @@
 # https://auth0.com/blog/image-processing-in-python-with-pillow/
 from PIL import Image
 import os
-
+import PIL as Pil
 
 def getFiles():
     file_l = []
-    for root, dirs, files in os.walk(".", topdown=False):
-        for name in files:
-            # print(name)
-            file_l.append(name)
-        #   print(os.path.join(root, name))
-        # for name in dirs:
-        #   print(os.path.join(root, name))
+    oldStyle=False
+
+    if oldStyle:
+        for root, dirs, files in os.walk(".", topdown=False):
+            for name in files:
+                # print(name)
+                file_l.append(name)
+            #   print(os.path.join(root, name))
+            # for name in dirs:
+            #   print(os.path.join(root, name))
+    else:
+        file_l = os.listdir()
     return file_l
 def genNewName(nameIn,suffix):
     nameParts = nameIn.split(".")
@@ -19,7 +24,7 @@ def genNewName(nameIn,suffix):
     return nameOut
 def isImage(nameIn):
     output=False
-    extIn = nameIn.split(".")[1]
+    extIn = nameIn.split(".")[-1]
     imgExtensions = ["JPG","jpg","png"]
     for i in imgExtensions:
         if extIn == i:
@@ -33,7 +38,8 @@ if __name__ == "__main__":
     for i in file_l:
         if isImage(i):
             image = Image.open(i)
+            image = Pil.ImageOps.exif_transpose(image)
             image.thumbnail((1500,1500))
             print(image.size)
-            image.save(genNewName(i,"_resize"))
+            image.save('gen/'+genNewName(i,"_r"))
     print("done.")
